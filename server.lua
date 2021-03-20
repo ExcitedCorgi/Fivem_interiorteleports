3918d94316546165a2b323ebd5ee212895400525
ExcitedCorgi <59341232+ExcitedCorgi@users.noreply.github.com>
2021-03-19 20:15:58 -0700

    Add files via upload

---
+++ server.lua
@@ -0,0 +1,95 @@
+RegisterCommand("interiortp", function(source, args)
+    if args[1] == "doomsday" then
+        SetEntityCoords(source, 483.2, 4810.5, -58.9, false, false, false, false)
+    elseif args[1] == "doomsdaysub" then
+        SetEntityCoords(source, 514.2, 4885.8, -62.5, false, false, false, false)
+    elseif args[1] == "iaafacility" then
+        SetEntityCoords(source, 2151.1, 2921.3, -61.9, false, false, false, false)
+    elseif args[1] == "iaaserver" then
+        SetEntityCoords(source, 2158.1, 2920.9, -81.0, false, false, false, false)
+    elseif args[1] == "hangar" then
+        SetEntityCoords(source, -1266.9, -3014.6, -49.5, false, false, false, false)
+    elseif args[1] == "nightclub" then
+        SetEntityCoords(source, -1569.4, -3016.9, -75.4, false, false, false, false)
+    elseif args[1] == "clubhouse1" then
+        SetEntityCoords(source, 1120.9, -3152.2, -37.0, false, false, false, false)
+    elseif args[1] == "clubhouse2" then
+        SetEntityCoords(source, 997.8, -3158.4, -38.9, false, false, false, false)
+    elseif args[1] == "cocaine" then
+        SetEntityCoords(source, 1088.6, -3188.2, -38.1, false, false, false, false)
+    elseif args[1] == "cocash" then
+        SetEntityCoords(source, 1137.9, -3198.5, -39.0, false, false, false, false)
+    elseif args[1] == "methlab" then
+        SetEntityCoords(source, 997.4, -3200.7, -36.0, false, false, false, false)
+    elseif args[1] == "weedfarm" then
+        SetEntityCoords(source, 1065.3, -3183.2, -39.0, false, false, false, false)
+    elseif args[1] == "office1" then
+        SetEntityCoords(source, -141.1, -620.9, 168.8, false, false, false, false)
+    elseif args[1] == "office2" then
+        SetEntityCoords(source, -75.8, -826.9, 243.3, false, false, false, false)
+    elseif args[1] == "office3" then
+        SetEntityCoords(source, -1579.7, -565.0, 108.5, false, false, false, false)
+    elseif args[1] == "office4" then
+        SetEntityCoords(source, -1392., -480.4, 72.0, false, false, false, false)
+    elseif args[1] == "bunker" then
+        SetEntityCoords(source, 848.6, 2996.5, 45.8, false, false, false, false)
+    elseif args[1] == "largegarage1" then
+        SetEntityCoords(source, -197.6, -580.6, -136.0, false, false, false, false)
+    elseif args[1] == "largegarage2" then
+        SetEntityCoords(source, -90.1, -821.1 ,222.0, false, false, false, false)
+    elseif args[1] == "largegarage3" then
+        SetEntityCoords(source, -1585.0, -582.8, 86.5, false, false, false, false)
+    elseif args[1] == "largegarage4" then
+        SetEntityCoords(source, -1394.4, -480.7, -49.1, false, false, false, false)
+    elseif args[1] == "modshop" then
+        SetEntityCoords(source, -72.1, -812.4, 285.1, false, false, false, false)
+    elseif args[1] == "vehware" then
+        SetEntityCoords(source, 971.0, -2990.0, -39.6, false, false, false, false)
+    elseif args[1] == "apthi1" then
+        SetEntityCoords(source, -18.9, -581.7, 90.2, false, false, false, false)
+    elseif args[1] == "apthi2" then
+        SetEntityCoords(source, -1485.8, -521.5, 57.0, false, false, false, false)
+    elseif args[1] == "aptmid1" then
+        SetEntityCoords(source, -1453.7, -537.6, 74.0, false, false, false, false)
+    elseif args[1] == "aptmid2" then
+        SetEntityCoords(source, -915.4, -366.3, 114.2, false, false, false, false)
+    elseif args[1] == "aptmid3" then
+        SetEntityCoords(source, -606.0, 59.3, 98.2, false, false, false, false)
+    elseif args[1] == "aptmid4" then
+        SetEntityCoords(source, -781.6, 323.0, 212.0, false, false, false, false)
+    elseif args[1] == "aptmid5" then
+        SetEntityCoords(source, -28.7, -597.0, 80.0, false, false, false, false)
+    elseif args[1] == "aptmid6" then
+        SetEntityCoords(source, -606.5, 59.3, -182.3, false, false, false, false)
+    elseif args[1] == "aptlow1" then
+        SetEntityCoords(source, -781.7, 319.3, 217.6, false, false, false, false)
+    elseif args[1] == "aptlow2" then
+        SetEntityCoords(source, -779.0, 338.6, 196.5, false, false, false, false)
+    elseif args[1] == "aptlow3" then
+        SetEntityCoords(source, -781.8, 318.6, 187.8, false, false, false, false)
+    elseif args[1] == "househi1" then
+        SetEntityCoords(source, -173.9, 496.9, 137.7, false, false, false, false)
+    elseif args[1] == "househi2" then
+        SetEntityCoords(source, 340.9, 437.1, 149.3, false, false, false, false)
+    elseif args[1] == "househi3" then
+        SetEntityCoords(source, 373.5, 423.1, 145.9, false, false, false, false)
+    elseif args[1] == "househi4" then
+        SetEntityCoords(source, -681.7, 591.9, 145.4, false, false, false, false)
+    elseif args[1] == "househi5" then
+        SetEntityCoords(source, -759.4, 618.8, 144.1, false, false, false, false)
+    elseif args[1] == "househi6" then
+        SetEntityCoords(source, -859.7, 690.1, 152.8, false, false, false, false)
+    elseif args[1] == "househi7" then
+        SetEntityCoords(source, 117.5, 559.0, 184.3, false, false, false, false)
+    elseif args[1] == "housemid" then
+        SetEntityCoords(source, 266.0, -1007.3, -101.0, false, false, false, false)
+    elseif args[1] == "houselow" then
+        SetEntityCoords(source, 346.4, -1012.0, -99.2, false, false, false, false)
+    elseif args[1] == "unionvault" then
+        SetEntityCoords(source, 6.3, -707.7, 16.1, false, false, false, false)
+    elseif args[1] == "morgue" then
+        SetEntityCoords(source, 274.4, -1360.3, 24.5, false, false, false, false)
+    else
+        TriggerClientEvent('chatMessage', source, {255, 25, 25}, "INVALID LOCATION! Valid Locations:", { 255, 25, 25}, "doomsday doomsdaysub iaafacility iaaserver hangar nightclub unionvault morgue cocash methlab modshop vehware weedfarm bunker cocaine houselow housemid clubhouse1-2 aptlow1-3 largegarage 1-4 largegarage1-4 office1-4 aptmid1-6 househi1-7")
+    end
+end, false)
